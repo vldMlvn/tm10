a1c6e6f1245ad48995451007f1b3764a9c60be47
StasMaster <56350576+StasMaster@users.noreply.github.com>
2023-07-03 20:40:23 +0200

    Update CityCollection.java

--- src/main/java/citycollection/CityCollection.java
+++ src/main/java/citycollection/CityCollection.java
@@ -21,22 +21,6 @@ public class CityCollection {
     }
 
     public void markCityUsed(String city) {
-        if (cityList.contains(city)) {
-            cityList.remove(city);
-            usedCityList.add(city);
-        } else {
-            System.out.println("Місто відсутнє у списку доступних міст.");
-        }
-    }
-
-    // Приклад використання класу CityCollection та тестування методiв
-    public static void main(String[] args) {
-        CityCollection collection = new CityCollection();
-        System.out.println(collection.getCityList());  // Виведе: [Київ, Нью-Йорк, Вашингтон]
-        System.out.println(collection.getUsedCityList());  // Виведе: []
-
-        collection.markCityUsed("Київ");
-        System.out.println(collection.getCityList());  // Виведе: [Нью-Йорк, Вашингтон]
-        System.out.println(collection.getUsedCityList());  // Виведе: [Київ]
+        usedCityList.add(city);
     }
 }
